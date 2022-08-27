c426b22f25e82b5c01c9e4980b7a07e34fc7093d
Stefan Koev <st.koev@outlook.com>
2022-08-27 13:45:14 +0300

    implement javascript logic, final commit

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 //using selectors inside the element
 // traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+// const btnArr = Array.from(btns);
+// console.log(btnArr);
+// btnArr.map((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+  console.log(question);
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
